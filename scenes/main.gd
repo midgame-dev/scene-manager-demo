ef8a42ffe107d7336e3c104fd6d03f707e4d6671
Drew Larson <116033515+drewlarso@users.noreply.github.com>
2024-07-27 21:24:27 -0700

    made project simpler

--- scenes/main.gd
+++ scenes/main.gd
@@ -8,14 +8,10 @@ static var _instance: GameManager
 func _ready() -> void:
 	if _instance == null:
 		_instance = self
-	else:
-		push_warning("More than one GameManager instance detected. Only one should exist.")
 
 static func change_scene(scene_path: String) -> void:
 	if _instance:
 		_instance.call_deferred("_deferred_change_scene", scene_path)
-	else:
-		push_error("GameManager instance not found. Ensure it's properly initialized.")
 
 func _deferred_change_scene(scene_path) -> void:
 	var new_scene = load(scene_path).instantiate()
